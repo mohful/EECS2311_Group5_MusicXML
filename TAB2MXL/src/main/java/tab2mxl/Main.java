cbf5398c32f3c8831c38dd725db61e9b75ce1dbd
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-01-16 16:43:27 -0500

    Adding Last names to show that i am learning.

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>(); 
-		list.add("Rafael");
-		list.add("Mohammed");
-		list.add("Nick");
-		list.add("Shawn");
-		list.add("Yashraj");
+		List<String> list = new ArrayList<>();
+		list.add("Rafael Dolores");
+		list.add("Mohammed Fulwala");
+		list.add("Nick Park");
+		list.add("Shawn I forgot your last name, sorry");
+		list.add("Yashraj I forgot your last name, sorry");
 		for (String e: list) { 
 			System.out.println("Member name: ");
 			System.out.println(e);
