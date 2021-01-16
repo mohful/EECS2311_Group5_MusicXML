03c4657f76b02bbbfba85eb858bea8108243f8b8
YUJunhyeongPark <oakr0414@my.yorku.ca>
2021-01-16 17:36:20 -0500

    Made a small change to show that I'm learning (took isValid function outside of main function)

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -4,6 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+	
+	/**
+	 * Given a string s containing just the characters,
+	 * '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+	 */
+	
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') stack.push(s.charAt(i));
+			if (s.charAt(i) == '[') stack.push(s.charAt(i));
+			if (s.charAt(i) == '{') stack.push(s.charAt(i));
+			if (s.charAt(i) == ')' ) {
+				if (stack.empty() || stack.peek() != '(') return false;
+				else stack.pop();
+			}
+			if (s.charAt(i) == ']' ) {
+				if (stack.empty() || stack.peek() != '[') return false;
+				else stack.pop();
+			}
+			if (s.charAt(i) == '}' ) {
+				if (stack.empty() || stack.peek() != '{') return false;
+				else stack.pop();
+			}
+		}
+		return stack.empty();
+	}
+	
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
 		list.add("Rafael Dolores");
@@ -15,31 +43,6 @@ public class Main {
 			System.out.println("Member name: ");
 			System.out.println(e);
 		}
-		public boolean isValid("((]]([}}{")
-		/**
-		 * Given a string s containing just the characters,
-		 * '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-		 */
-		public boolean isValid(String s) {
-			Stack<Character> stack = new Stack<>();
-			for (int i = 0; i < s.length(); i++) {
-				if (s.charAt(i) == '(') stack.push(s.charAt(i));
-				if (s.charAt(i) == '[') stack.push(s.charAt(i));
-				if (s.charAt(i) == '{') stack.push(s.charAt(i));
-				if (s.charAt(i) == ')' ) {
-					if (stack.empty() || stack.peek() != '(') return false;
-					else stack.pop();
-				}
-				if (s.charAt(i) == ']' ) {
-					if (stack.empty() || stack.peek() != '[') return false;
-					else stack.pop();
-				}
-				if (s.charAt(i) == '}' ) {
-					if (stack.empty() || stack.peek() != '{') return false;
-					else stack.pop();
-				}
-			}
-			return stack.empty();
-		}
+		public boolean isValid("((]]([}}{");
 	}
 }
