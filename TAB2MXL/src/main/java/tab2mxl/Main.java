d34a56e8ac0c8f577f6f8288e5cc02e186e054a1
Rafael Dolores <rafd47@my.yorku.ca>
2021-01-20 11:03:32 -0500

    HAI

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -7,12 +7,52 @@ import java.util.Stack;
 public class Main {
 	
 	protected void check() { 
-		System.out.println("Hi");
+		System.out.println("Hi professo");
+	}
+	/**
+	 * Given a string s containing just the characters,
+	 * '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+	 */
+	public static boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') stack.push(s.charAt(i));
+			if (s.charAt(i) == '[') stack.push(s.charAt(i));
+			if (s.charAt(i) == '{') stack.push(s.charAt(i));
+			if (s.charAt(i) == ')' ) {
+				if (stack.empty() || stack.peek() != '(') return false;
+				else stack.pop();
+			}
+			if (s.charAt(i) == ']' ) {
+				if (stack.empty() || stack.peek() != '[') return false;
+				else stack.pop();
+			}
+			if (s.charAt(i) == '}' ) {
+				if (stack.empty() || stack.peek() != '{') return false;
+				else stack.pop();
+			}
+		}
+		return stack.empty();
 	}
 	
-	protected void check2() {
-		System.out.println("Hello");
-	}
+	/**
+	 * Given an array nums and a value val, 
+	 * remove all instances of that value in-place and return the new length.
+	 * @param nums
+	 * @param val
+	 * @return
+	 */
+	
+	public static int removeElement(int[] nums, int val) {
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[length] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
 	
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
@@ -25,5 +65,12 @@ public class Main {
 			System.out.println("Member name: ");
 			System.out.println(e);
 		}
+		System.out.println(isValid("((]]([}}{"));
+		int[] arr = {1, 2, 3, 4, 5};
+		System.out.println(removeElement(arr, 2));
+	}
+	
+	protected void confirmWorking() { 
+		System.out.println("Hi");
 	}
 }
