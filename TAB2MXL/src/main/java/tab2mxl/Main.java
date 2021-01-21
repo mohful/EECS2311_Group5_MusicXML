b9d74d6b3c7061e71140064b4f36bcc52522e515
YUJunhyeongPark <oakr0414@my.yorku.ca>
2021-01-21 14:40:16 -0500

    Fixed Main.java by deleting unnecessary codes and fixing syntax error. Also deleted unnecessary abstraction

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -2,23 +2,6 @@ package tab2mxl;
 
 import button.GUI;
 
-public class Main extends Application{
-	
-	protected void check() { 
-		System.out.println("Hi professo");
-		System.out.println("Yeah Hai");
-		System.out.println("Rafael");
-		System.out.println("Mohammed");
-		System.out.println("Junhyeong");
-		System.out.println("Yashraj");
-		System.out.println("Shawn");
-	}
-	
-	
-	protected void check2() { 
-		System.out.println("Test");
-		System.out.println("Yeah Hai");
-	}
 public class Main{
 	
 	public static void main(String[] args) {
